4484e45713938bb7a74a13f9fa82c0654473394e
TsybulskaAnna <tsybulska.an@gmail.com>
2022-04-18 17:27:14 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,36 @@
 import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
+const FORM_DATA_NAME = 'feedback-form-state';
+const data = { email: '', message: '' };
 
-const formData = {};
+populateIntupFeilds(form);
 
-function onFormData(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+function populateIntupFeilds(form) {
+  if (localStorage.getItem(FORM_DATA_NAME)) {
+    const dataReceived = JSON.parse(localStorage.getItem(FORM_DATA_NAME));
+
+    form.elements.email.value = dataReceived.email;
+    form.elements.message.value = dataReceived.message;
+  }
+}
+function onFormData({event}) {
+  localStorage.setItem(FORM_DATA_NAME, updateData(event));
+}
+
+function updateData(event) {
+  if (event.target.name === 'email') {
+    data.email = event.target.value;
+  }
+  if (event.target.name === 'message') {
+    data.message = event.target.value;
+  }
+  return data;
 }
 
 function onSubmitForm(e) {
@@ -23,18 +44,7 @@ function onSubmitForm(e) {
   
 }
 
-
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
-  }
-})();
-
-
-console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-e.currentTarget.reset();
-localStorage.removeItem('feedback-form-state');}
+console.log( updateData(event));
+event.currentTarget.reset();
+localStorage.removeItem(FORM_DATA_NAME);
+}
